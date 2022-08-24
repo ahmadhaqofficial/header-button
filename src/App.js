3cd123c7bc3dc4d5d5dce7261bdeeec9bc5ae285
Ahmad haq <106380578+ahmadhaqofficial@users.noreply.github.com>
2022-08-24 01:06:57 -0700

    react app done

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import {
+  BrowserRouter,
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Switch,
+} from "react-router-dom";
+import Home from "./screens/Home.jsx";
+
+function App() {
+  return;
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </BrowserRouter>;
+}
+
+export default App;
