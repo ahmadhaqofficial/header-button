a5524cf55dcefe26111e7a57ce941938a919fb99
Ahmad haq <106380578+ahmadhaqofficial@users.noreply.github.com>
2024-01-10 09:19:08 -0800

    today

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import "./App.scss";
 import Logo from "../assets/logo.png";
 import Logo2 from "../assets/logo2.png";
+import Download from "../assets/download.png";
+import Store from "../assets/store.png";
+import Arrow from "../assets/arrow.png";
+import Pizza from "../assets/menu.png";
+import Apple from "../assets/apple.png";
 import { useLayoutEffect } from "react";
 import OutsideClickHandler from "react-outside-click-handler";
 
@@ -101,7 +106,43 @@ export default function Home() {
         </div>
       
     </div>
-   
+   <div className="menu__main__container">
+    <div className="menu__left__container">
+     <div className="menu__content__left">
+     <img className="download__img" src={Download} alt="" />
+      <div className="menu__contents__container">
+      
+      Hello Menu is available <br /> for your android or <br /> Apple.
+      
+      </div>
+      <div>
+        <div className="store__container">
+          <img className="store__img" src={Store} alt="" />
+          <div className="store__content">
+            Get it on <br /><span>Play store</span>
+
+          </div>
+        </div>
+        <div className="Apple__container">
+          <img src={Apple} alt="" />
+          <div className="Apple__content">
+            Get it on <br /> <span className="play__store__content">Play store
+</span>
+          </div>
+        </div>
+      </div>
+     </div>
+     
+    </div>
+    <div className="arrow__img__container">
+      <img className="arrow__img" src={Arrow} alt="" />
+
+      </div>
+    <div className="menu__right__container">
+<img className="pizza__img" src={Pizza} alt="" />
+
+    </div>
+   </div>
     </>
   );
 }
